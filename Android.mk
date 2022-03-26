d6d436729201ab6af3eb994ef4ae01645751973d
nift4 <nift4@protonmail.com>
2022-03-26 20:59:48 +0100

    rework

--- Android.mk
+++ Android.mk
@@ -1,5 +1,3 @@
 MY_PATH := vendor/magisk
 MAGISK_OUT := $(PRODUCT_OUT)/obj/MAGISK
 
-$(MAGISK_OUT)/magisk.zip:
-	export ANDROID_BUILD_TOP=$$(pwd) MAGISK_OUT=$$(cd $(MAGISK_OUT); pwd) && $${ANDROID_BUILD_TOP}/$(MY_PATH)/build_magisk.sh
--- build_magisk.sh
+++
@@ -1,4 +0,0 @@
-/usr/bin/python3 -m venv $MAGISK_OUT/venv
-. $MAGISK_OUT/venv/bin/activate
-cd $ANDROID_BUILD_TOP/vendor/magisk/src
-./build.py -r -o $MAGISK_OUT/main_obj all
